ff5e7e43c5178cfba651e60eb2383b4efa2f54ae
manila <3621543+manila@users.noreply.github.com>
2024-02-19 11:41:20 -0500

    docusaurus.config.js

--- blog/docusaurus.config.js
+++ blog/docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
           {
             href: 'projects/',
             label: 'Projects',
-            position: 'Left',
+            position: 'left',
           },
         ],
       },
