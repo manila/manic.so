da636167c837c6556fffd1a00d0fc29eb38440d3
manila <3621543+manila@users.noreply.github.com>
2024-03-21 01:25:35 -0400

    now: add profile image

--- pages/now/index.js
+++ pages/now/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import Layout from '@theme/Layout';
-import { useBlogPost } from '@docusaurus/theme-common/internal';
+import styles from './styles.module.css';
 
 function Space() {
   return (<><p dangerouslySetInnerHTML={{__html: '&nbsp;'}} /></>);
@@ -12,17 +12,19 @@ export default function Now() {
     <Layout>
       <main className={clsx('container margin-vert--xl')}>
         <div className="row">
-          <div className="col col--6 col--offset-3">
-            <Heading as="h1" className="hero__title">Manuel Nila</Heading>
-            <Heading as="h2">/now</Heading>
+          <div className="col col--6 col--offset-4">
+            <img src="img/profile.png" className={styles.profile}/>
             <Space />
-            <Heading as="h3">Profession:</Heading>
+            <Heading as="h1" className="hero__title">Manuel Nila</Heading>
+            <Heading as="h2" className="hero__subtitle">/now</Heading>
+            <Space />
+            <Heading as="h2">Profession:</Heading>
             <p>Engineering @ BuzzFeed</p>
             <Space />
-            <Heading as="h3">Specialty:</Heading>
+            <Heading as="h2">Specialty:</Heading>
             <p>Cloud Engineering (Kubernetes & GCP)</p>
             <Space />
-            <Heading as="h3">Links:</Heading>
+            <Heading as="h2">Links:</Heading>
             <p><a href="https://manic.so/projects">manic.so/projects</a></p>
             <p><a href="https://github.com/manila">github.com/manila</a></p>
             <p><a href="https://glitch.com/@manila">glitch.com/@manila</a></p>
